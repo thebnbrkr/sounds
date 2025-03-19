2411c4f22835d1d545254859175926b7fcfc12dc
progsama <imtiaznasif@gmail.com>
2025-03-18 22:06:19 -0700

    updated files
    Updated Killzone and made lives manager in both scene 1 and 2

--- Assets/_Scripts/Logic/KillZone.cs
+++ Assets/_Scripts/Logic/KillZone.cs
@@ -4,6 +4,18 @@ public class KillZone : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Ball")) GameManager.Instance.KillBall();
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            // Find the LivesManager in the scene and call LoseLife()
+            LivesManager livesManager = FindObjectOfType<LivesManager>();
+            if (livesManager != null)
+            {
+                livesManager.LoseLife();
+            }
+            else
+            {
+                Debug.LogWarning("LivesManager not found in scene.");
+            }
+        }
     }
 }
